0b36a9eeb64434ac15c5ef1001253a6b8a994c5d
christianhasselstrom <crellecrelle@gmail.com>
2015-11-25 14:09:35 +0100

    HomeScreen og Login

--- src/GUI/HomeScreen.java
+++ src/GUI/HomeScreen.java
@@ -1,185 +1,46 @@
 package GUI;
 
-import Database.SchemaCreator;
-import Database.TableCreator;
-import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.Reflection;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import sun.rmi.runtime.Log;
 
 /**
- * Created by christianhasselstrom on 16/11/2015.
+ * Created by christianhasselstrom on 25/11/2015.
  */
-/*
-public class GUI extends Application {
+public class HomeScreen {
 
-    BorderPane root, borderRoot;
-    GridPane gridRoot;
-    Stage window;
-    ToggleGroup tGroup;
-    ToggleButton myProfileBtn, sellersBtn, buyersBtn, matchesBtn, tasksBtn,  signOutBtn;
-    Button loginButton, createSellerButton, createBuyerButton;
-    Label headLine, passLabel, userLabel, vicarius, invisibleLabel;
-    VBox topVBox, loginVBox, usersVBox;
-    HBox buttonBox, loginHBox, topHBox, usersHBox;
-    TextField loginTextfield;
-    PasswordField passwordField;
-
-
-    public void GUI()
-    {
-        //BorderPane
-
-
-    }
-
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        SchemaCreator.create();
-        TableCreator.create();
-        window = primaryStage;
-
-        Login.LoginZ();
-    }
-
-    public void Login()
-    {
-        root = new BorderPane();
-        root.setPadding(new Insets(15));
-        root.setStyle("-fx-background-color: linear-gradient(#42C0FB, #236B8E) ");
-
-        loginVBox = new VBox();
-        loginVBox.setPrefWidth(300);
-        loginVBox.setSpacing(10);
-        root.setTop(loginVBox);
-
-        topHBox = new HBox();
-        topHBox.setPrefWidth(300);
-        topHBox.setSpacing(30);
-
-        loginHBox = new HBox();
-        loginHBox.setPrefWidth(300);
-        loginHBox.setSpacing(30);
-
-        //Users HBox
-        usersHBox = new HBox();
-        usersHBox.setPrefWidth(1280);
-        usersHBox.setSpacing(10);
-
-        //Users VBox
-        usersVBox = new VBox();
-        usersVBox.setPrefWidth(1280);
-        usersVBox.setSpacing(10);
-        invisibleLabel = new Label();
-        root.setCenter(usersVBox);
-        usersVBox.setPadding(new Insets(150));
-
-
-        window.show();
-        Scene scene = new Scene(root, 550, 600);
-        window.setScene(scene);
-        window.setResizable(false);
-
-        //user Label
-        userLabel = new Label("Login");
-        userLabel.setPrefWidth(200);
-
-        //
-        passLabel = new Label("Password");
-        passLabel.setPrefWidth(200);
-
-        //login textfield
-        loginTextfield = new TextField();
-        loginTextfield.setPrefWidth(200);
-        loginTextfield.setPrefHeight(30);
-
-        //login passwordfield
-        passwordField = new PasswordField();
-        passwordField.setPrefWidth(200);
-        passwordField.setPrefHeight(30);
-
-        //login button
-        loginButton = new Button("Login");
-
-        //Create seller button
-        createSellerButton = new Button("Create Seller");
-        createSellerButton.setPrefWidth(250);
-        createSellerButton.setPrefHeight(65);
-        createSellerButton.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
-        createSellerButton.setOnAction(e ->
-        {
-
-        });
-
-        //create buyer button
-        createBuyerButton = new Button("Create Buyer");
-        createBuyerButton.setPrefWidth(250);
-        createBuyerButton.setPrefHeight(65);
-        createBuyerButton.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
-
-        //vicarius label
-        vicarius = new Label("Vicarius");
-        Reflection r = new Reflection();
-        r.setFraction(0.7f);
-        vicarius.setEffect(r);
-        vicarius.setTextFill(Color.LIGHTGRAY);
-        vicarius.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
-
-
-
-        //getChildren()
-        loginVBox.getChildren().addAll(vicarius, topHBox, loginHBox);
-        topHBox.getChildren().addAll(userLabel, passLabel);
-        loginHBox.getChildren().addAll(loginTextfield, passwordField, loginButton);
-        //usersHBox.getChildren().addAll(createBuyerButton, createSellerButton);
-        usersVBox.getChildren().addAll(createBuyerButton, createSellerButton);
-
-
-    }
-
-   /* public void homeScreen()
-    {
-        root = new BorderPane();
+    public static void homeScreen() {
+        BorderPane root = new BorderPane();
+        ToggleGroup tGroup = new ToggleGroup();
         root.setStyle("-fx-background-color: linear-gradient(#42C0FB, #236B8E) ");
         root.setPadding(new Insets(15));
-        topVBox = new VBox();
+        VBox topVBox = new VBox();
         topVBox.setPrefWidth(1280);
         topVBox.setSpacing(70);
         root.setTop(topVBox);
-        buttonBox = new HBox();
+        HBox buttonBox = new HBox();
         buttonBox.setPrefWidth(1280);
         buttonBox.setSpacing(5);
-        window.show();
+
 
         //Create scene
+        Stage window = new Stage();
         Scene scene = new Scene(root, 1280, 700);
         window.setScene(scene);
         window.setResizable(false);
-
-
-
+        window.show();
 
 
         //Label
-        headLine = new Label("Vicarius");
+        Label headLine = new Label("Vicarius");
         Reflection r = new Reflection();
         r.setFraction(0.7f);
         headLine.setEffect(r);
@@ -187,7 +48,7 @@ public class GUI extends Application {
         headLine.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
 
         //sellersBtn
-        sellersBtn = new ToggleButton("Sellers");
+        ToggleButton sellersBtn = new ToggleButton("Sellers");
         sellersBtn.setPrefHeight(20);
         sellersBtn.setPrefWidth(100);
         sellersBtn.setFont(Font.font("Verdana"));
@@ -199,7 +60,7 @@ public class GUI extends Application {
         });
 
         //buyersBtn
-        buyersBtn = new ToggleButton("Buyers");
+        ToggleButton buyersBtn = new ToggleButton("Buyers");
         buyersBtn.setPrefHeight(20);
         buyersBtn.setPrefWidth(100);
         buyersBtn.setFont(Font.font("Verdana"));
@@ -211,7 +72,7 @@ public class GUI extends Application {
         });
 
         //matchesBtn
-        matchesBtn = new ToggleButton("Matches");
+        ToggleButton matchesBtn = new ToggleButton("Matches");
         matchesBtn.setPrefHeight(20);
         matchesBtn.setPrefWidth(100);
         matchesBtn.setFont(Font.font("Verdana"));
@@ -223,7 +84,7 @@ public class GUI extends Application {
         });
 
         //taskBtn
-        tasksBtn = new ToggleButton("Tasks");
+        ToggleButton tasksBtn = new ToggleButton("Tasks");
         tasksBtn.setPrefHeight(20);
         tasksBtn.setPrefWidth(100);
         tasksBtn.setFont(Font.font("Verdana"));
@@ -235,7 +96,7 @@ public class GUI extends Application {
         });
 
         //myProfileBtn
-        myProfileBtn = new ToggleButton("My Profile");
+        ToggleButton myProfileBtn = new ToggleButton("My Profile");
         myProfileBtn.setPrefHeight(20);
         myProfileBtn.setPrefWidth(100);
         myProfileBtn.setFont(Font.font("Verdana"));
@@ -248,13 +109,13 @@ public class GUI extends Application {
         });
 
         //signOutBtn
-        signOutBtn = new ToggleButton("Sign Out");
+        ToggleButton signOutBtn = new ToggleButton("Sign Out");
 
 
         topVBox.getChildren().addAll(headLine, buttonBox);
     }
-    public TableView sellersTable()
-    {
+
+    public static TableView sellersTable() {
         TableView sellersTable = new TableView();
 
         sellersTable.setPrefWidth(400);
@@ -275,8 +136,7 @@ public class GUI extends Application {
         return sellersTable;
     }
 
-    public TableView buyersTable()
-    {
+    public static TableView buyersTable() {
         TableView buyersTable = new TableView();
 
         buyersTable.setPrefWidth(400);
@@ -295,8 +155,7 @@ public class GUI extends Application {
         return buyersTable;
     }
 
-    public TableView matchesTable()
-    {
+    public static TableView matchesTable() {
         TableView matchesTable = new TableView();
 
         matchesTable.setPrefWidth(400);
@@ -319,7 +178,7 @@ public class GUI extends Application {
         return matchesTable;
     }
 
-    public TableView tasksTable() {
+    public static TableView tasksTable() {
         TableView tasksTable = new TableView();
 
         tasksTable.setPrefWidth(400);
@@ -341,4 +200,5 @@ public class GUI extends Application {
 
         return tasksTable;
     }
-}*/
+
+}
--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -1,21 +1,30 @@
 package GUI;
 
+import Database.SchemaCreator;
+import Database.TableCreator;
+import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Reflection;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import sun.security.util.Password;
 
 /**
  * Created by christianhasselstrom on 25/11/2015.
  */
-public class Login
-{
+public class Login extends Application {
     BorderPane borderRoot;
     Stage window;
     HBox loginHBox;
@@ -23,28 +32,106 @@ public class Login
     TextField loginTextfield;
     PasswordField passwordField;
 
-
-    public void loginZ()
+    @Override
+    public void start(Stage primaryStage) throws Exception
     {
-        borderRoot = new BorderPane();
-        Scene sceneLogin = new Scene(borderRoot, 600, 600);
-        window.setScene(sceneLogin);
+        SchemaCreator.create();
+        TableCreator.create();
+        window = primaryStage;
 
-        loginHBox = new HBox();
-
-        loginButton = new Button("Login");
-
-        loginTextfield = new TextField();
-        passwordField = new PasswordField();
+        Login();
+    }
 
 
-        loginHBox.setSpacing(10);
-        borderRoot.setTop(loginHBox);
-        loginHBox.setPrefWidth(600);
+    public static void Login() {
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(15));
+        root.setStyle("-fx-background-color: linear-gradient(#42C0FB, #236B8E) ");
 
-        loginHBox.getChildren().addAll(loginTextfield, passwordField, loginButton);
-        borderRoot.getChildren().addAll(loginHBox);
+        VBox loginVBox = new VBox();
+        loginVBox.setPrefWidth(300);
+        loginVBox.setSpacing(10);
+        root.setTop(loginVBox);
 
+        HBox topHBox = new HBox();
+        topHBox.setPrefWidth(300);
+        topHBox.setSpacing(30);
+
+        HBox loginHBox = new HBox();
+        loginHBox.setPrefWidth(300);
+        loginHBox.setSpacing(30);
+
+        //Users HBox
+        HBox usersHBox = new HBox();
+        usersHBox.setPrefWidth(1280);
+        usersHBox.setSpacing(10);
+
+        //Users VBox
+        VBox usersVBox = new VBox();
+        usersVBox.setPrefWidth(1280);
+        usersVBox.setSpacing(10);
+        root.setCenter(usersVBox);
+        usersVBox.setPadding(new Insets(150));
+
+
+        Stage window = new Stage();
         window.show();
+        Scene scene = new Scene(root, 550, 600);
+        window.setScene(scene);
+        window.setResizable(false);
+
+        //user Label
+        Label userLabel = new Label("Login");
+        userLabel.setPrefWidth(200);
+
+        //
+        Label passLabel = new Label("Password");
+        passLabel.setPrefWidth(200);
+
+        //login textfield
+        TextField loginTextfield = new TextField();
+        loginTextfield.setPrefWidth(200);
+        loginTextfield.setPrefHeight(30);
+
+        //login passwordfield
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPrefWidth(200);
+        passwordField.setPrefHeight(30);
+
+        //login button
+        Button loginButton = new Button("Login");
+
+        //Create seller button
+        Button createSellerButton = new Button("Create Seller");
+        createSellerButton.setPrefWidth(250);
+        createSellerButton.setPrefHeight(65);
+        createSellerButton.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
+        createSellerButton.setOnAction(e ->
+        {
+            window.close();
+
+        });
+
+        //create buyer button
+        Button createBuyerButton = new Button("Create Buyer");
+        createBuyerButton.setPrefWidth(250);
+        createBuyerButton.setPrefHeight(65);
+        createBuyerButton.setStyle("-fx-background-color: linear-gradient(#fafdfe, #a7d9f5)");
+
+        //vicarius label
+        Label vicarius = new Label("Vicarius");
+        Reflection r = new Reflection();
+        r.setFraction(0.7f);
+        vicarius.setEffect(r);
+        vicarius.setTextFill(Color.LIGHTGRAY);
+        vicarius.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
+
+
+        //getChildren()
+        loginVBox.getChildren().addAll(vicarius, topHBox, loginHBox);
+        topHBox.getChildren().addAll(userLabel, passLabel);
+        loginHBox.getChildren().addAll(loginTextfield, passwordField, loginButton);
+        //usersHBox.getChildren().addAll(createBuyerButton, createSellerButton);
+        usersVBox.getChildren().addAll(createBuyerButton, createSellerButton);
     }
 }
